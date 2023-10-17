b44b1fbd920ca44a806102debfdb557cbdb95f21
cvt220106 <1564851464@qq.com>
2023-10-17 19:28:19 +0800

    agg_func: add the null type process in count3!

--- src/observer/sql/operator/project_physical_operator.cpp
+++ src/observer/sql/operator/project_physical_operator.cpp
@@ -239,8 +239,10 @@ RC ProjectPhysicalOperator::compute_aggregation(AggType type, Value &ans, const 
     }
     case AGG_COUNT: {
        if (val.attr_type() == NULLS) {
-        ans.set_type(INTS);
-        ans.set_int(0);
+        if (ans.attr_type() == UNDEFINED) {
+          ans.set_type(INTS);
+          ans.set_int(0);
+        }
         break;
        }
        if (ans.attr_type() == UNDEFINED) {
