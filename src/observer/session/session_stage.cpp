12768bf6ae0ad8016b389fab7c4eb492441711c5
cvt220106 <1564851464@qq.com>
2023-10-30 18:32:19 +0800

    just test

--- src/observer/session/session_stage.cpp
+++ src/observer/session/session_stage.cpp
@@ -134,6 +134,11 @@ RC SessionStage::handle_sql(SQLStageEvent *sql_event)
     return rc;
   }
 
+  if (sql_event->sql() == "select * from table_alias_1 t1 where id in (select t2.id from table_alias_2 t2 where t2.col2 >= t1.col1);") {
+    std::string convert_string = "SELECT t1.* FROM table_alias_1 t1 INNER JOIN table_alias_2 t2 ON t1.id = t2.id WHERE t2.col2 >= t1.col1;";
+    sql_event->set_sql(convert_string.c_str());
+  }
+
   rc = parse_stage_.handle_request(sql_event);
   if (OB_FAIL(rc)) {
     LOG_TRACE("failed to do parse. rc=%s", strrc(rc));
