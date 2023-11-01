067f9a89d281dd5aaa91f5b924b366752fa15eda
leauny <leauny@outlook.com>
2023-11-01 10:03:56 +0800

    fix: connection buffer size.

--- src/observer/common/ini_setting.h
+++ src/observer/common/ini_setting.h
@@ -22,6 +22,6 @@ See the Mulan PSL v2 for more details. */
 #define PORT "PORT"
 #define PORT_DEFAULT 6789
 
-#define SOCKET_BUFFER_SIZE 8192
+#define SOCKET_BUFFER_SIZE 81920
 
 #define SESSION_STAGE_NAME "SessionStage"
